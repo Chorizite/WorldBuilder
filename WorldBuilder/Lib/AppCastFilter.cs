7c40933ece71e92ec04d2dadb7cf711b66180247
trevis <trevis@haxit.org>
2025-10-06 00:06:24 -0600

    Fix AutoUpdater versioning

--- WorldBuilder/Lib/AppCastFilter.cs
+++ WorldBuilder/Lib/AppCastFilter.cs
@@ -8,49 +8,32 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace WorldBuilder.Lib {
-    /// <summary>
-    /// Basic <seealso cref="IAppCastFilter"/> implementation for filtering
-    /// your app cast items based on a channel name (e.g. "beta"). Makes it
-    /// easy to allow your users to be on a beta software track or similar.
-    /// Note that a "stable" channel search string will not be interpreted as versions 
-    /// like "1.0.0"; it will look for versions like "1.0.0-stable1" (aka search for
-    /// the string "stable").
-    /// Names are compared in a case-insensitive manner.
-    /// </summary>
     public class OSAppCastFilter : IAppCastFilter {
         private ILogger? _logWriter;
 
         public OSAppCastFilter(ILogger? logWriter = null) {
-            RemoveOlderItems = true;
             _logWriter = logWriter;
-            OSName = "windows-x64";
         }
+        public string OSName => "windows-x64";
 
-        public bool RemoveOlderItems { get; set; }
-
-        public string OSName { get; set; }
-
-        public bool IsEdgeChannel => App.Version.Contains("+");
+        public string Channel => App.Version.Contains("+") ? "edge" : "stable";
 
         /// <inheritdoc/>
         public IEnumerable<AppCastItem> GetFilteredAppCastItems(SemVerLike installed, IEnumerable<AppCastItem> items) {
             return items.Where((item) => {
                 var semVer = SemVerLike.Parse(item.Version);
                 var appCastItemChannel = item.Channel ?? "";
-                if (RemoveOlderItems && semVer.CompareTo(installed) <= 0) {
-                    _logWriter?.PrintMessage("Removing older item from filtered app cast results");
+
+                // Filter out other channels
+                if (Channel != appCastItemChannel) {
                     return false;
                 }
 
-
-                // Ignore edge channels if we are not on an edge channel
-                if (!IsEdgeChannel && appCastItemChannel.Contains("edge")) {
+                // Filter out older versions
+                if (semVer.CompareTo(installed) <= 0) {
                     return false;
                 }
 
-                if (string.IsNullOrWhiteSpace(OSName)) {
-                    return true;
-                }
                 return item.OperatingSystem?.ToLower() == OSName.ToLower();
             }).OrderByDescending(x => x.SemVerLikeVersion);
         }
